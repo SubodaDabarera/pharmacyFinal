c333c6945cd069172da24ec0e5b3a3d45ee2283f
Suboda Dabarera <88880898+SubodaDabarera@users.noreply.github.com>
2022-07-16 02:35:09 +0530

    modal and routes added

--- server/models/InventoryModel.js
+++ server/models/InventoryModel.js
@@ -1,36 +1,41 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const inventorySchema = new mongoose.Schema({
-    name:{
-        type:String,
-        require: true
-    },
-    price:{
-        type:Number
-    },
-    item_code:{
-        type: Number
-    },
-    quantity:{
-        type: Number
-    },
-    exp_date: {
-        type: Date,
-        require: true
-    },
-    batchid:{
-        type: Number,
-    },
-    description:{
-        type: String
-    },
-    stockimage:{
-        type:String
-    },
-    numReviews:{
-        type: Number}, 
-    rating: {
-        type: Number}
-})
+  name: {
+    type: String,
+    require: true,
+  },
+  price: {
+    type: Number,
+  },
+  item_code: {
+    type: Number,
+  },
+  quantity: {
+    type: Number,
+  },
+  exp_date: {
+    type: Date,
+    require: true,
+  },
+  batchid: {
+    type: Number,
+  },
+  description: {
+    type: String,
+  },
+  stockimage: {
+    type: String,
+  },
+  numReviews: {
+    type: Number,
+  },
+  rating: {
+    type: Number,
+  },
+  category: {
+    type: String,
+  },
+});
 
-module.exports = mongoose.model("Inventories", inventorySchema)
+module.exports = mongoose.model("Inventories", inventorySchema);
--- server/routes/productRoute.js
+++ server/routes/productRoute.js
@@ -1,10 +1,11 @@
-const router = require('express').Router()
-const {model} = require('mongoose')
-const productControl = require('../controllers/productControl')
+const router = require("express").Router();
+const { model } = require("mongoose");
+const productControl = require("../controllers/productControl");
 
-router.get('/products', productControl.getProducts)
-router.patch('/products/:id', productControl.reviews)
+router.get("/products", productControl.getProducts);
+router.patch("/products/:id", productControl.reviews);
 
+// get product of categories
+router.get("/products/category", productControl.filterProduct);
 
-
-module.exports = router
+module.exports = router;
